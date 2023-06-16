699d304b2a86b2084f3b085ddf5a0fd1ec96439e
Rohit Paul <113459757+RohitPaul12345@users.noreply.github.com>
2023-06-16 07:45:23 +0000

    Config The File

---
+++ .devcontainer/code base/test.js
@@ -0,0 +1,11 @@
+//Create-Pattern-Of-Stars
+function createPattern(numberoofrows){
+    for (var i=0;i<numberofrows;i++){
+        var stringtoprint= "";
+        for (var j=0;j<i+1;j++){
+            stringtoprint= stringtoprint + "*"
+        }
+        console.log(stringtoprint)
+    }
+}
+createPattern(100)
